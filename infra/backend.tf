cb5353e8e6182d51e0d5d9a73d1e3b149b8d67b4
isaque524 <isaquesilvadamasceno@gmail.com>
2025-12-07 16:05:27 -0300

    stage

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
+    resource_group_name  = "rg-tfstage"
     storage_account_name = "tfstatecurso10019"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
