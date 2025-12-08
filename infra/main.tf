1a333a64a66cec9a430be56f1d82be7611525a58
isaque524 <isaquesilvadamasceno@gmail.com>
2025-12-08 19:49:24 -0300

    formatacao terraform

--- infra/main.tf
+++ infra/main.tf
@@ -49,27 +49,27 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-  name                       = "SSH"
-  priority                   = 1001
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "22"
-  source_address_prefix      = "0.0.0.0/0"
-  destination_address_prefix = "0.0.0.0/0"
-}
-security_rule {
-  name                       = "Swagger"
-  priority                   = 1002
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "8081"
-  source_address_prefix      = "0.0.0.0/0"
-  destination_address_prefix = "0.0.0.0/0"
-}
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "0.0.0.0/0"
+  }
+  security_rule {
+    name                       = "Swagger"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8081"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "0.0.0.0/0"
+  }
 }
 
 
